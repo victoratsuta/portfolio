1d09f29158eec516cdc4312c5672dee68083e5ea
victor_my_dev_rep <vovan.kurnov@mail.ru>
2018-02-11 15:03:00 +0300

    add params for mobile browsers

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -15,7 +15,11 @@
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name=apple-mobile-web-app-status-bar-style content=black>
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Atsuta Victor | Web Master</title>
 
