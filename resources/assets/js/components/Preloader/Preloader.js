084effba620123d3fd25bb9e99b7e96e386c6486
victoratsuta <atsutavictor.dev@gmail.com>
2018-11-03 16:07:48 +0800

    set gitignore config

--- resources/assets/js/components/Preloader/Preloader.js
+++ resources/assets/js/components/Preloader/Preloader.js
@@ -31,7 +31,7 @@ class Preloader extends Component {
                     <div className="ip-loader">
                         <svg className="ip-inner" width="60px" height="60px" viewBox="0 0 80 80">
                             <path className="ip-loader-circlebg"
-                                  d="M40,10C57.351,10,71,23.649,71,40.5S57.351,71,40.5,71 S10,57.351,10,40.5S23.649,10,40.5,10z"/>
+                                  d="M40,10C57. 351,10,71,23.649,71,40.5S57.351,71,40.5,71 S10,57.351,10,40.5S23.649,10,40.5,10z"/>
                             <path id="ip-loader-circle" className="ip-loader-circle"
                                   d="M40,10C57.351,10,71,23.649,71,40.5S57.351,71,40.5,71 S10,57.351,10,40.5S23.649,10,40.5,10z"/>
                         </svg>
